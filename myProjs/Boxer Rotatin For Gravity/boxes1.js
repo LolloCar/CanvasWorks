b9575a5befe29ae50ee3a75844a9bdef8797ca74
LolloCar <86184891+LolloCar@users.noreply.github.com>
2021-12-28 20:01:37 +0100

    initial sketch of boxes1

--- myProjs/Boxer Rotatin For Gravity/boxes1.js
+++ myProjs/Boxer Rotatin For Gravity/boxes1.js
@@ -1,33 +1,59 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
 
-const size = 1080;
-
 const settings = {
-  dimensions: [ size,size ]
+  dimensions: [ 1080,1080 ]
 };
 
-//Variables
 
-boxSize =  size * 0.2;
 
 const sketch = ({ context, width, height }) => {
 	
+  //Variables
+  Box.size =  width * 0.2;
  
-  const box_Distance = boxSize * 1.1; 
+  //Distance between boxes (NOT the mortar!)
+  const gap = Box.size  * 1.6; 
+  const lattice_side = 2;
+  let lattice = makeLattice(lattice_side,lattice_side,gap);
+  latticeSize = gap * (lattice_side-1) + Box.size;
+  console.log('Box.size = '+Box.size+',gap='+gap+' latticeSize = '+latticeSize);
+  //center the lattice
+  translateVectorArray(lattice,(width-latticeSize)/2+Box.size/2,(width-latticeSize)/2+Box.size/2);
   
-
-  let box1 = new Box(200,200,random.range(-Math.PI,Math.PI));
-  
+  let box1 = new Box(lattice[0],random.range(-Math.PI,Math.PI));
+  let box2 = new Box(lattice[1],random.range(-Math.PI,Math.PI));
+  let box3 = new Box(lattice[2],random.range(-Math.PI,Math.PI));
+  let box4 = new Box(lattice[3],0);
+  let drawables = [box1,box2,box3,box4];
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 	context.fillStyle = 'black';
-	box1.draw(context);
+	drawables.forEach(drawable => {drawable.draw(context);})
   };
 };
 
 canvasSketch(sketch, settings);
+/*
+* Creates a lattice of vectors, its top left centered on origin
+*/
+function makeLattice(rows,columns,gap)
+{
+	let lattice = [];
+	for (let i =0; i < columns; i++) 
+	{ for (let j = 0; j < rows; j++) 
+		{
+			lattice.push(new Vector(i*gap,j*gap));
+		}
+	}
+	return lattice;
+}	
+
+function translateVectorArray(array,x,y) 
+{
+	array.forEach(item => {item.x += x, item.y += y});
+}
 
 class Vector {
 	constructor(x,y) {
@@ -43,26 +69,30 @@ class Vector {
 	}
 }
 
-
-
 //rotation 0 = points up
 class Box {
-	
-	//var box_Side = 0;
-
-	
-	constructor (x,y,rotation,side) {
-		this.x = x;
-		this.y = y;
+	static size = 0;
+	constructor (position, rotation) {
+		this.pos = position;
 		this.rotation = rotation;
 	}
 	
 	draw(context) {
 		context.save();
-		context.translate(this.x,this.y);
-		context.rotate(this.rotation);
-		context.fillRect(-boxSize/2,-boxSize/2,boxSize,boxSize);
+		context.translate(this.pos.x,this.pos.y);
+		context.rotate(this.rotation-Math.PI/2);
+		context.fillRect(-Box.size/2,-Box.size/2,Box.size,Box.size);
+		context.fillStyle='red';
+		circle(context,Box.size/4,0,5);
 		context.restore();
 		
 	}
+}
+
+const circle = (context,centerX,centerY,radius) => {
+	context.save();
+	context.beginPath();
+	context.arc(centerX,centerY,radius,0,Math.PI*2);
+	context.fill();
+	context.restore();
 }
